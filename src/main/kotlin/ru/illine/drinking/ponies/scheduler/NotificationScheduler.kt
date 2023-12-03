42468f4cf2682318a3a2e9aae5fa45799dac6f8a
Евгения Ковтун <eekovtun@gmail.com>
2023-12-03 22:02:32 +0300

    CQCP-21: added canceling of notifications

--- src/main/kotlin/ru/illine/drinking/ponies/scheduler/NotificationScheduler.kt
+++ src/main/kotlin/ru/illine/drinking/ponies/scheduler/NotificationScheduler.kt
@@ -7,6 +7,7 @@ import ru.illine.drinking.ponies.bot.DrinkingPoniesTelegramBot
 import ru.illine.drinking.ponies.dao.access.NotificationAccessService
 import ru.illine.drinking.ponies.model.dto.NotificationDto
 import java.time.OffsetDateTime
+import java.util.stream.Collectors
 
 @Component
 class NotificationScheduler(
@@ -14,17 +15,24 @@ class NotificationScheduler(
     private val drinkingPoniesTelegramBot: DrinkingPoniesTelegramBot
 ) {
 
+    private val MAX_NOTIFICATION_ATTEMPTS = 3
+    private val FOR_NOTIFICATIONS = false
+    private val FOR_CANCEL = true
+
     private val log = LoggerFactory.getLogger("SCHEDULER")
 
-    @Scheduled(fixedRate = 300000)
+    //@Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRate = 30000)
     fun drinkingNotification() {
         log.info("The Drinking Notification Scheduler is started...")
-        notificationAccessService.findAll()
+        val notifications = notificationAccessService.findAll()
             .stream()
             .filter { compareLastSentNotification(it) }
-            .forEach {
-                drinkingPoniesTelegramBot.notify(it.chatId)
-            }
+            .collect(Collectors.partitioningBy { it.notificationAttempts == MAX_NOTIFICATION_ATTEMPTS })
+        notifyAll(notifications)
+        cancelAll(notifications)
+
+        log.info("The Drinking Notification Scheduler is finished")
     }
 
     private fun compareLastSentNotification(it: NotificationDto): Boolean {
@@ -32,4 +40,19 @@ class NotificationScheduler(
         return whenSendNotification.toEpochSecond() < OffsetDateTime.now().toEpochSecond()
     }
 
+    private fun notifyAll(notifications: Map<Boolean, List<NotificationDto>>) {
+        log.info("Sending notifications all users")
+        val forNotifying = notifications.getValue(FOR_NOTIFICATIONS)
+        log.info("The notifications will be sent [{}] users", forNotifying.size)
+
+        drinkingPoniesTelegramBot.sendNotifications(forNotifying)
+    }
+
+    private fun cancelAll(notifications: Map<Boolean, List<NotificationDto>>) {
+        log.info("Cancel (suspend) notification")
+        val forSuspending = notifications.getValue(FOR_CANCEL)
+        log.info("The notifications will be suspended for [{}] users", forSuspending.size)
+
+        drinkingPoniesTelegramBot.suspendNotifications(forSuspending)
+    }
 }
