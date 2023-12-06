405ae07c34e7fac1a5db6e7263e22c7a960d5c44
Evgeniya Kovtun <eekovtun@gmail.com>
2023-12-06 07:31:08 +0300

    CQCP-28: added availability turn on/off any notifications (#7)

--- src/main/kotlin/ru/illine/drinking/ponies/dao/repository/NotificationRepository.kt
+++ src/main/kotlin/ru/illine/drinking/ponies/dao/repository/NotificationRepository.kt
@@ -1,11 +1,27 @@
 package ru.illine.drinking.ponies.dao.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import ru.illine.drinking.ponies.model.entity.NotificationEntity
 
 interface NotificationRepository : JpaRepository<NotificationEntity, Long> {
 
+    @Query(
+        value = "select n.* from notifications n where n.user_id = :userId",
+        nativeQuery = true
+    )
+    fun findAnyByUserId(@Param("userId") userId: Long): NotificationEntity?
+
     fun findByUserId(userId: Long): NotificationEntity?
 
-    fun existsByUserId(userId: Long): Boolean
+    fun deleteByUserId(userId: Long): Long
+
+    @Modifying
+    @Query(
+        value = "update notifications set deleted = false where user_id = :userId",
+        nativeQuery = true
+    )
+    fun enableByUserId(@Param("userId") userId: Long)
 }
