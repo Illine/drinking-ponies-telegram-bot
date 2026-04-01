4b178b997380808d2eb990a21a533d5dd279b1b0
Evgeniya Kovtun <eekovtun@gmail.com>
2026-04-01 12:41:12 +0300

    DPTB-23: add WaterAmountApplyReplyButtonStrategy with test

---
+++ src/main/kotlin/ru/illine/drinking/ponies/service/button/strategy/wateramount/WaterAmountApplyReplyButtonStrategy.kt
@@ -0,0 +1,68 @@
+package ru.illine.drinking.ponies.service.button.strategy.wateramount
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery
+import org.telegram.telegrambots.meta.generics.TelegramClient
+import ru.illine.drinking.ponies.dao.access.NotificationAccessService
+import ru.illine.drinking.ponies.dao.access.WaterStatisticAccessService
+import ru.illine.drinking.ponies.model.base.AnswerNotificationType
+import ru.illine.drinking.ponies.model.base.WaterAmountType
+import ru.illine.drinking.ponies.model.dto.internal.WaterStatisticDto
+import ru.illine.drinking.ponies.service.button.ReplyButtonStrategy
+import ru.illine.drinking.ponies.service.telegram.MessageEditorService
+import ru.illine.drinking.ponies.util.telegram.TelegramMessageConstants
+import java.time.Clock
+import java.time.LocalDateTime
+
+@Service
+class WaterAmountApplyReplyButtonStrategy(
+    private val sender: TelegramClient,
+    private val notificationAccessService: NotificationAccessService,
+    private val waterStatisticAccessService: WaterStatisticAccessService,
+    private val messageEditorService: MessageEditorService,
+    private val clock: Clock
+) : ReplyButtonStrategy {
+
+    private val logger = LoggerFactory.getLogger("REPLY-STRATEGY")
+
+    override fun reply(callbackQuery: CallbackQuery) {
+        messageEditorService.deleteReplyMarkup(
+            callbackQuery.message.chatId,
+            callbackQuery.message.messageId
+        )
+
+        val userId = callbackQuery.from.id
+        val chatId = callbackQuery.message.chatId
+        val queryData = callbackQuery.data
+
+        val waterAmountType = WaterAmountType.typeOf(queryData) ?: WaterAmountType.ML_250
+
+        logger.info(
+            "A telegram user [{}] for telegram chat [{}] drank [{}] ml of water",
+            userId,
+            chatId,
+            waterAmountType.amountMl
+        )
+
+        val notificationSetting = notificationAccessService.updateTimeOfLastNotification(userId, LocalDateTime.now(clock))
+
+        waterStatisticAccessService.save(
+            WaterStatisticDto(
+                telegramUser = notificationSetting.telegramUser,
+                eventTime = LocalDateTime.now(clock),
+                eventType = AnswerNotificationType.YES,
+                waterAmountMl = waterAmountType.amountMl
+            )
+        )
+
+        SendMessage(
+            chatId.toString(),
+            TelegramMessageConstants.NOTIFICATION_ANSWER_YES_MESSAGE
+        ).apply { sender.execute(this) }
+    }
+
+    override fun isQueryData(queryData: String): Boolean = WaterAmountType.typeOf(queryData) != null
+
+}
