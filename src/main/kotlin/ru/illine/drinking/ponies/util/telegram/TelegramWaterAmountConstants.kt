e4a6395df6f5483040a703aa4982d683f5f576e1
Evgeniya Kovtun <eekovtun@gmail.com>
2026-04-05 17:13:12 +0300

    DPTB-83: add 'const' for val in TelegramWaterAmountConstants.kt

--- src/main/kotlin/ru/illine/drinking/ponies/util/telegram/TelegramWaterAmountConstants.kt
+++ src/main/kotlin/ru/illine/drinking/ponies/util/telegram/TelegramWaterAmountConstants.kt
@@ -2,10 +2,10 @@ package ru.illine.drinking.ponies.util.telegram
 
 object TelegramWaterAmountConstants {
 
-    val ML_50 = "50 мл"
-    val ML_100 = "100 мл"
-    val ML_150 = "150 мл"
-    val ML_250 = "250 мл"
-    val ML_450 = "450 мл"
+    const val ML_50 = "50 мл"
+    const val ML_100 = "100 мл"
+    const val ML_150 = "150 мл"
+    const val ML_250 = "250 мл"
+    const val ML_450 = "450 мл"
 
 }
